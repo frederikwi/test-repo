e78319efa8d673173096fbffabeba8c9296b92eb
frederikwi <65390704+frederikwi@users.noreply.github.com>
2020-05-19 18:50:10 +0200

    Added stateSpaceMatrices.py - File containing the state space matrices in the LQG feedback cooling model using the Rotating Wave Approximation

---
+++ stateSpaceMatrices.py
@@ -0,0 +1,44 @@
+##############################################
+# Matricerne fra State-space model
+##############################################
+
+
+import numpy as np
+
+def inputMatrixA(Omega_m,Gamma_m,kappa,g):
+    return np.array([
+        [-Gamma_m,Omega_m,0,0],
+        [-Omega_m,-Gamma_m,-2*g,0],
+        [0,0,-kappa/2,0],
+        [-2*g,0,0,-kappa/2],
+    ])
+
+def inputMatrixB(kappa_in):
+    return np.array([
+        [0,0],
+        [0,0],
+        [np.sqrt(kappa_in),0],
+        [0,np.sqrt(kappa_in)]
+    ])
+
+def inputMatrixC(theta,eta,kappa_out):
+    return np.sqrt(2*eta*kappa_out)*np.array(
+        [0,0,np.cos(theta),np.sin(theta)]
+    )
+
+
+def inputmatrixD(Gamma_m,kappa,nbar):
+    return np.array([
+        [2*Gamma_m*(nbar+0.5),0,0,0],
+        [0,2*Gamma_m*(nbar+0.5),0,0],
+        [0,0,kappa/2,0],
+        [0,0,0,kappa/2]
+    ])
+
+def inputmatrixE(Gamma_m,kappa,nbar):
+    return np.sqrt(inputmatrixD(Gamma_m,kappa,nbar))
+
+def inputmatrixGamma(theta,eta,kappa_out):
+    return np.sqrt(eta*kappa_out/2)*np.array(
+        [0,0,-cos(theta),-sin(theta)]
+    )
